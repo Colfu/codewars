4be0151c03349a572b103e199e7cc72edf5f02e5
Colfu <colfu@Colfus-MacBook-Air.local>
2021-07-24 18:57:06 +0100

    attempting commiting this from within bash terminal

--- 6th_kyu/statistics_for_running.py
+++ 6th_kyu/statistics_for_running.py
@@ -120,7 +120,7 @@ def stat(times_str):
 
 
 # some tests:
-print(stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"),
-      "Should be = Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
+# print(stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"),
+#       "Should be = Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
 # print(stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"),
 #       "Should be = Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")
