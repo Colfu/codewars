67f8af76fcb3635e9d99de27ca4b53876f38a68c
Colfu <colfu@Colfus-MacBook-Air.local>
2021-08-02 16:08:48 +0100

    Fixed broken variable name.

--- 6th_kyu/break_camelCase.py
+++ 6th_kyu/break_camelCase.py
@@ -21,10 +21,10 @@ def solution(camelCase_string):
         new_string [string]: String with camelCase removed
     """
     # start new string with index 0 so the following does not try to add space
-    new_string = s[0]               
+    new_string = camelCase_string[0]               
     
     # Ignore 1st letter incase of cap, add each letter to new string, and if Capital then add preceeding space
-    for x in s[1:]:                 
+    for x in camelCase_string[1:]:                 
         if x.islower():
             new_string += x         
         else:
