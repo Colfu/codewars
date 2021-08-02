79dcf910ceb313a0e344e2f80fe62b82c69c4d07
Colfu <colfu@Colfus-MacBook-Air.local>
2021-08-02 15:10:29 +0100

    Test added to TestAutocomplete.

--- 6th_kyu/6th_kyu_tests.py
+++ 6th_kyu/6th_kyu_tests.py
@@ -4,14 +4,25 @@ from autocomplete_yay import autocomplete
 
 class TestAutocomplete(unittest.TestCase):
 
-    
-
-    def test_remove_occurrences(self):
+    def test_filtering(self):
         dictionary = ['abnormal', 'Arm-wrestling', 'absolute', 'airplane', 'airport', 'amazing','apple', 'ball']
-
         self.assertEqual(autocomplete('ai', dictionary), ['airplane','airport'])
-        self.assertEqual(autocomplete('-a', dictionary), ['abnormal','Arm-wrestling','absolute','airplane','airport'])
 
+    def test_no_more_than_5(self):
+        dictionary = ['ab', 'ac', 'ad', 'ae', 'af', 'ag']
+        self.assertEqual(autocomplete('a', dictionary), ['ab', 'ac', 'ad', 'ae', 'af'])
+
+    def test_no_matches(self):
+        dictionary = ['c1', 'c2',]
+        self.assertEqual(autocomplete('b', dictionary), [])
+
+    def test_keep_same_order(self):
+        dictionary = ['ag', 'af', 'ae', 'ad']
+        self.assertEqual(autocomplete('a', dictionary), ['ag', 'af', 'ae', 'ad'])
+
+    def test_no_non_alpha_input(self):
+        dictionary = ['c1', 'c2',]
+        self.assertEqual(autocomplete('!c', dictionary), ['c1', 'c2'])
 
 
 
