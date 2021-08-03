23791736221ebc4d9a9e2f5d1a090fc31d197b78
Colfu <colfu@Colfus-MacBook-Air.local>
2021-08-03 11:54:50 +0100

    Created tests and updated function docstring.

--- 6th_kyu/6th_kyu_tests.py
+++ 6th_kyu/6th_kyu_tests.py
@@ -1,8 +1,9 @@
 import unittest
 
 from autocomplete_yay import autocomplete
+from break_camelCase import remove_camelCase
 
-class TestAutocomplete(unittest.TestCase):
+class Test_Autocomplete(unittest.TestCase):
 
     def test_filtering(self):
         dictionary = ['abnormal', 'Arm-wrestling', 'absolute', 'airplane', 'airport', 'amazing','apple', 'ball']
@@ -24,7 +25,13 @@ class TestAutocomplete(unittest.TestCase):
         dictionary = ['c1', 'c2',]
         self.assertEqual(autocomplete('!c', dictionary), ['c1', 'c2'])
 
+class Test_RemoveCamelCase(unittest.TestCase):
 
+    # tests:
+    # wordWord - basic
+    # WordWord - with first cap
+    # Word Word - already a space
+    pass
 
 
 
--- 6th_kyu/break_camelCase.py
+++ 6th_kyu/break_camelCase.py
@@ -1,25 +1,17 @@
-# Complete the solution so that the function will break up camel casing,
-# using a space between words.
-#
-# Example
-# "camelCasing"  =>  "camel Casing"
-# "identifier"   =>  "identifier"
-# ""             =>  ""
-# -----------------------------------------
-# Plan:
-# loop through string, add to new string
-# if case is cap, add with a preceding space
-# unsure whether it includes upper camel case as well, so sort out 1st letter in case
 
-def solution(camelCase_string):
+def remove_camelCase(camelCase_string):
     """ Break up camel casing, using a space between words.
 
-    Args:
-        camelCase_sting ([string]): Some words using camelCase
+    Example:
+    "camelCasing"  =>  "camel Casing"
+    "identifier"   =>  "identifier"
 
+    Args:
+        camelCase_sting [string]: Some words using camelCase
     Returns:
         new_string [string]: String with camelCase removed
     """
+    
     # start new string with index 0 so the following does not try to add space
     new_string = camelCase_string[0]               
     
