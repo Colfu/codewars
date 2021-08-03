f4e133166983347ea0657797fd7ee7a9e960f6c5
Colfu <colfu@Colfus-MacBook-Air.local>
2021-08-03 12:38:26 +0100

    Updated comnents in function, created new tests in test file.

--- 6th_kyu/6th_kyu_tests.py
+++ 6th_kyu/6th_kyu_tests.py
@@ -2,6 +2,7 @@ import unittest
 
 from autocomplete_yay import autocomplete
 from break_camelCase import remove_camelCase
+from unique_in_order import unique_in_order
 
 class Test_Autocomplete(unittest.TestCase):
 
@@ -27,14 +28,32 @@ class Test_Autocomplete(unittest.TestCase):
 
 class Test_RemoveCamelCase(unittest.TestCase):
 
-    # tests:
-    # wordWord - basic
-    # WordWord - with first cap
-    # Word Word - already a space
-    pass
+    def test_normal_camelCase(self):
+        self.assertEqual(remove_camelCase('wordWord'), 'word Word')
 
+    def test_ignore_initial_capital(self):
+        self.assertEqual(remove_camelCase('WordWord'), 'Word Word')
 
+    def test_ignore_if_preceeding_space(self):
+        self.assertEqual(remove_camelCase('word Word'), 'word Word')
 
+class Test_UniqueInOrder(unittest.TestCase):
 
+    def test_ints(self):
+        self.assertEqual(unique_in_order([1, 3, 3, 3, 2, 2, 1, 3]), [1, 3, 2, 1, 3])
 
+    def test_strings(self):
+        self.assertEqual(unique_in_order(['pen', 'car', 'pen', 'pen', 'lion', 'boat', 'boat', 'boat', 'boat', 'cup']), ['pen', 'car', 'pen', 'lion', 'boat', 'cup'])
+
+    def test_strings_and_ints(self): 
+        self.assertEqual(unique_in_order(['pen', 1, 1, 'car', 'car', 'car', 3567, 3567, 'pen', 'car']), ['pen', 1, 'car', 3567, 'pen', 'car'])
+
+    def test_empty_list(self):
+        self.assertEqual(unique_in_order([]), [])
+
+    def test_single_item(self):
+        self.assertEqual(unique_in_order(['single']), ['single'])
+
+    def test_double_item(self):
+        self.assertEqual(unique_in_order([2,2]), [2])
 
--- 6th_kyu/unique_in_order.py
+++ 6th_kyu/unique_in_order.py
@@ -1,14 +1,14 @@
 def unique_in_order(list_of_args):
     """
-    Turns a sequence into list without any same value elements next to each other, preserving the original order.
+    Turn a sequence into list without any same value elements next to each other, preserving the original order.
 
-    :param: list_of_args - input is list of arguments, str or int
+    :param: list_of_args: input is list of arguments, str or int
     :return: final_list_of_items
     """
-    # if input is empty, return empty list
+    # If input is empty, return empty list
     if len(list_of_args) < 1: return []
     else:
-        # setup 1st item to compare 2nd against
+        # Setup 1st item to compare 2nd against
         current_item = list_of_args[0]
         final_list_of_items = [list_of_args[0]]
         #  Go through each item, pass if same as current_item, otherwise add to final_list_of_items
